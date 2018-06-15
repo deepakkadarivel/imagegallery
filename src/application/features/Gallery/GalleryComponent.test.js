c4395006cc63a1576c6db2c4757c0ab65379f23e
deepakkadarivel <deepak.kadarivel@gmail.com>
2018-06-15 11:44:06 +0530

    [IG-010] Test rendering Masonary Grid with thumbnails.

--- src/application/features/Gallery/GalleryComponent.test.js
+++ src/application/features/Gallery/GalleryComponent.test.js
@@ -40,4 +40,57 @@ describe('<GalleryComponent />', () => {
       expect(loaderComponent.length).toBe(1);
     });
   });
+
+  describe('<Masonry>', () => {
+    it('renders the Masonry component', () => {
+      const component = shallow(<GalleryComponent {...props} />);
+      component.setProps({
+        gallery: {
+          thumbnails: [
+            {
+              id: '1',
+              type: 'image/png',
+              link: 'sample link',
+              title: 'image one'
+            },
+            {
+              id: '2',
+              type: 'image/gif',
+              link: 'sample link',
+              title: 'image two'
+            },
+            {
+              id: '3',
+              type: 'video/mp4',
+              link: 'sample link',
+              title: 'video one'
+            },
+            {
+              id: '4',
+              title: 'image three',
+              images: [
+                {
+                  type: 'image/jpeg',
+                  link: 'sample link'
+                }
+              ]
+            }
+          ],
+          promise: {
+            getGallery: setPromiseState(false, true, false)
+          }
+        }
+      });
+
+      let masonryComponent = component.find('Masonry');
+      expect(masonryComponent.length).toBe(1);
+      expect(masonryComponent.props().className).toBe('my-masonry-grid');
+      expect(masonryComponent.props().columnClassName).toBe(
+        'my-masonry-grid_column'
+      );
+
+      let thumbnailComponent = masonryComponent.find('Thumbnail');
+      expect(thumbnailComponent.length).toBe(3);
+    });
+  });
 });
