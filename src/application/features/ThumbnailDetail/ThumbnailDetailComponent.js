2acbcbf15ad73c98c3bb17aaefeabb8173208a0e
deepakkadarivel <deepak.kadarivel@gmail.com>
2018-06-16 08:23:01 +0530

    [IG-012] Render Thumbnail detail with votes and score

--- src/application/features/ThumbnailDetail/ThumbnailDetailComponent.js
+++ src/application/features/ThumbnailDetail/ThumbnailDetailComponent.js
@@ -6,15 +6,28 @@ class ThumbnailDetailComponent extends Component {
   render() {
     return (
       <div className="thumbnail-detail">
-        <div className="detail-card">
-          <img
-            src={this.props.selectedThumbnailImage}
-            alt={this.props.selectedThumbnail.title}
-          />
-          <div className="container">
-            <p>{this.props.selectedThumbnail.title}</p>
+        {this.props.selectedThumbnailImage && (
+          <div className="detail-card">
+            <img
+              src={this.props.selectedThumbnailImage}
+              alt={this.props.selectedThumbnail.title}
+            />
+            <div className="container">
+              <h5>{this.props.selectedThumbnail.title}</h5>
+              {this.props.selectedThumbnail.description && (
+                <p>{this.props.selectedThumbnail.description}</p>
+              )}
+              <p>
+                <span id="upvote" /> {this.props.selectedThumbnail.ups}
+                <span id="downvote" /> {this.props.selectedThumbnail.downs}
+                <span id="score" /> {this.props.selectedThumbnail.score}
+              </p>
+            </div>
           </div>
-        </div>
+        )}
+        {!this.props.selectedThumbnailImage && (
+          <div>Nothing to display. Select an image from Gallery</div>
+        )}
       </div>
     );
   }
--- src/application/features/ThumbnailDetail/ThumbnailDetailComponent.test.js
+++ src/application/features/ThumbnailDetail/ThumbnailDetailComponent.test.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { shallow } from 'enzyme';
 
 import ThumbnailDetailComponent from './ThumbnailDetailComponent';
+import setPromiseState from '../../shared/utilities/promiseState';
 
 describe('<ThumbnailDetailComponent />', () => {
   const props = {
@@ -11,9 +12,52 @@ describe('<ThumbnailDetailComponent />', () => {
   it('renders the ThumbnailDetail Component', () => {
     const component = shallow(<ThumbnailDetailComponent {...props} />);
     expect(component.hasClass('thumbnail-detail')).toBeTruthy();
+  });
+
+  it('renders the details if selectedThumbnailImage prop exists', () => {
+    const component = shallow(<ThumbnailDetailComponent {...props} />);
+    expect(component.hasClass('thumbnail-detail')).toBeTruthy();
+
+    component.setProps({
+      selectedThumbnail: {
+        id: '123',
+        title: 'image title',
+        description: 'description',
+        ups: 12,
+        downs: 10,
+        score: 123
+      },
+      selectedThumbnailImage: 'http://sample_image_ref.png'
+    });
 
     const detailCard = component.find('.detail-card');
     expect(detailCard.length).toBe(1);
-    expect(detailCard.find('img').length).toBe(1);
+
+    const image = detailCard.find('img');
+    expect(image.length).toBe(1);
+    expect(image.props().src).toBe('http://sample_image_ref.png');
+    expect(image.props().alt).toBe('image title');
+
+    const container = detailCard.find('.container');
+    expect(container.find('h5').props().children).toBe('image title');
+    expect(
+      container
+        .find('p')
+        .first()
+        .props().children
+    ).toBe('description');
+
+    const voteParagraph = container.find('p').last();
+    expect(voteParagraph.find('span').length).toBe(3);
+  });
+
+  it('renders no content message if selectedThumbnailImage prop does not exists', () => {
+    const component = shallow(<ThumbnailDetailComponent {...props} />);
+    expect(component.hasClass('thumbnail-detail')).toBeTruthy();
+
+    const message = component.find('div').last();
+    expect(message.props().children).toBe(
+      'Nothing to display. Select an image from Gallery'
+    );
   });
 });
